6713072fc1a3e5939fd026d56a4d6fce4a23be57
dreace@foxmail.com <Dreace@Foxmail.com>
2022-12-25 11:55:31 +0800

    日志中打印真实客户端 IP

--- fasthttplogger.go
+++ fasthttplogger.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"log"
+	"net"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/valyala/fasthttp"
@@ -51,6 +53,31 @@ func getHttp(ctx *fasthttp.RequestCtx) string {
 	return "HTTP/1.0"
 }
 
+func getRealRemoteIP(ctx *fasthttp.RequestCtx) string {
+	realIP := ctx.Request.Header.Peek("X-Real-IP")
+	if len(realIP) > 0 {
+		ip := net.ParseIP(string(realIP))
+		if ip != nil {
+			if !ip.IsGlobalUnicast() {
+				return ip.String()
+			}
+		}
+	}
+	xForwardedFor := ctx.Request.Header.Peek("X-Forwarded-For")
+	if len(xForwardedFor) != 0 {
+		parts := strings.Split(string(xForwardedFor), ",")
+		if len(parts) != 0 {
+			ip := net.ParseIP(parts[0])
+			if ip != nil {
+				if !ip.IsGlobalUnicast() {
+					return ip.String()
+				}
+			}
+		}
+	}
+	return ctx.RemoteIP().String()
+}
+
 /* ========================== Predefined Formats =========================== */
 
 // Tiny format:
@@ -86,15 +113,16 @@ func TinyColored(req fasthttp.RequestHandler) fasthttp.RequestHandler {
 }
 
 // Short format:
-// <remote-addr> | <HTTP/:http-version> | <method> <url> - <status> - <response-time us>
-// 127.0.0.1:53324 | HTTP/1.1 | GET /hello - 200 - 44.8µs
+// <remote-addr> | <real-remote-ip> | <HTTP/:http-version> | <method> <url> - <status> - <response-time us>
+// 127.0.0.1:53324 | 1.1.1.1 | HTTP/1.1 | GET /hello - 200 - 44.8µs
 func Short(req fasthttp.RequestHandler) fasthttp.RequestHandler {
 	return fasthttp.RequestHandler(func(ctx *fasthttp.RequestCtx) {
 		begin := time.Now()
 		req(ctx)
 		end := time.Now()
-		output.Printf("%v | %s | %s %s - %v - %v",
+		output.Printf("%v | %s | %s | %s %s - %v - %v",
 			ctx.RemoteAddr(),
+			getRealRemoteIP(ctx),
 			getHttp(ctx),
 			ctx.Method(),
 			ctx.RequestURI(),
@@ -110,8 +138,9 @@ func ShortColored(req fasthttp.RequestHandler) fasthttp.RequestHandler {
 		begin := time.Now()
 		req(ctx)
 		end := time.Now()
-		output.Printf("%v | %s | %s %s - %v - %v",
+		output.Printf("%v | %s | %s | %s %s - %v - %v",
 			ctx.RemoteAddr(),
+			getRealRemoteIP(ctx),
 			getHttp(ctx),
 			colorMethod(ctx.Method(), ctx.Response.Header.StatusCode()),
 			ctx.RequestURI(),
@@ -122,16 +151,17 @@ func ShortColored(req fasthttp.RequestHandler) fasthttp.RequestHandler {
 }
 
 // Combined format:
-// [<time>] <remote-addr> | <HTTP/http-version> | <method> <url> - <status> - <response-time us> | <user-agent>
+// [<time>] <remote-addr> | <real-remote-ip> | <HTTP/http-version> | <method> <url> - <status> - <response-time us> | <user-agent>
 // [2017/05/31 - 13:27:28] 127.0.0.1:54082 | HTTP/1.1 | GET /hello - 200 - 48.279µs | Paw/3.1.1 (Macintosh; OS X/10.12.5) GCDHTTPRequest
 func Combined(req fasthttp.RequestHandler) fasthttp.RequestHandler {
 	return fasthttp.RequestHandler(func(ctx *fasthttp.RequestCtx) {
 		begin := time.Now()
 		req(ctx)
 		end := time.Now()
-		output.Printf("[%v] %v | %s | %s %s - %v - %v | %s",
+		output.Printf("[%v] %v | %s | %s | %s %s - %v - %v | %s",
 			end.Format("2006/01/02 - 15:04:05"),
 			ctx.RemoteAddr(),
+			getRealRemoteIP(ctx),
 			getHttp(ctx),
 			ctx.Method(),
 			ctx.RequestURI(),
@@ -148,9 +178,10 @@ func CombinedColored(req fasthttp.RequestHandler) fasthttp.RequestHandler {
 		begin := time.Now()
 		req(ctx)
 		end := time.Now()
-		output.Printf("[%v] %v | %s | %s %s - %v - %v | %s",
+		output.Printf("[%v] %v | %s | %s | %s %s - %v - %v | %s",
 			end.Format("2006/01/02 - 15:04:05"),
 			ctx.RemoteAddr(),
+			getRealRemoteIP(ctx),
 			getHttp(ctx),
 			colorMethod(ctx.Method(), ctx.Response.Header.StatusCode()),
 			ctx.RequestURI(),
